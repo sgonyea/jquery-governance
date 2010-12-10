4e9dd7cf44c2063298e2bbec2e4cc0d7eafeae7e
Scott Gonyea <me@aitrus.org>
2010-12-10 12:51:01 -0800

    The time -> timestamp migration was blowing up PG:
    ==  TimesBegone: migrating ====================================================
    -- change_column(:active_memberships, :start_time, :datetime)
    rake aborted!
    An error has occurred, this and all later migrations canceled:
    
    PGError: ERROR:  column "start_time" cannot be cast to type timestamp without time zone
    : ALTER TABLE "active_memberships" ALTER COLUMN "start_time" TYPE timestamp

--- db/migrate/20101128015610_create_active_memberships.rb
+++ db/migrate/20101128015610_create_active_memberships.rb
@@ -2,7 +2,7 @@ class CreateActiveMemberships < ActiveRecord::Migration
   def up
     create_table :active_memberships do |t|
       t.belongs_to :member
-      t.time :start_time, :end_time
+      t.timestamp :start_time, :end_time
 
       t.timestamps
     end
